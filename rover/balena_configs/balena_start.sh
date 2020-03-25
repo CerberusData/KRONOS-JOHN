386abc3d0fab7fbc8dd8029ce5c7b82166a5c2f4
John Betancourt <john.betancourt@javeriana.edu.co>
2020-03-25 14:32:07 -0500

    [WIP]: turn jetson fans

--- rover/balena_configs/balena_start.sh
+++ rover/balena_configs/balena_start.sh
@@ -3,13 +3,18 @@
 # Download from nvidia packages to build GPU containers for balena
 # Davidnet (david@kiwibot.com)
 # JohnBetaCode (john@kiwibot.com)
-set -euo pipefail
 
-# nvpmodel -m 0
-# /usr/src/app/nvidia_utils/jetson_clocks.sh
+# Safe bash script
+set -euo pipefail 
+
+# Use the maximum frequency of the CPU
+nvpmodel -m 0
+
+# Turn on the jetson fan
+/usr/src/app/jetson_clocks.sh
 
 # xinit &
-# wget https://s3.amazonaws.com/kiwibot/eyes.mp4
+wget https://s3.amazonaws.com/kiwibot/eyes.mp4 -nc -q
 # while true ; do gst-launch-1.0 filesrc location=eyes.mp4 !  qtdemux name=demux demux.video_0 ! queue ! h264parse ! omxh264dec !  nvoverlaysink display-id=0 -e ; done
 
 sleep infinity
