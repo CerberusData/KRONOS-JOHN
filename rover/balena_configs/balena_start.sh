5a6a8dd2d1811f9dd28f60fc74d6272be03c3420
John Betancourt <john.betancourt@javeriana.edu.co>
2020-03-27 22:33:19 -0500

    [WIP]: ROS2 source setup

--- rover/balena_configs/balena_start.sh
+++ rover/balena_configs/balena_start.sh
@@ -5,16 +5,18 @@
 # JohnBetaCode (john@kiwibot.com)
 
 # Safe bash script
-set -euo pipefail 
+# set -euo pipefail 
 
 # Use the maximum frequency of the CPU
-nvpmodel -m 0
+# nvpmodel -m 0 --verbose
 
 # Turn on the jetson fan
-/usr/src/app/jetson_clocks.sh
+# jetson_clocks.sh
 
-# xinit &
-wget https://s3.amazonaws.com/kiwibot/eyes.mp4 -nc -q
-# while true ; do gst-launch-1.0 filesrc location=eyes.mp4 !  qtdemux name=demux demux.video_0 ! queue ! h264parse ! omxh264dec !  nvoverlaysink display-id=0 -e ; done
+# wget https://s3.amazonaws.com/kiwibot/eyes.mp4 -nc -q
+nvgstplayer-1.0 -i /data/eyes.mp4 --svd="omxh264dec" --svs="nvoverlaysink # display-id=0" --loop-forever
 
-sleep infinity
+# Source ROS2 and execute ROS launch
+echo "source /opt/ros/dashing/setup.bash" >> ~/.bashrc || true
+
+sleep infinity 
