14101ebf4b61ccfa9a9d8701f18f5268e557b28e
John Betancourt <john.betancourt@javeriana.edu.co>
2020-03-30 17:41:11 -0500

    [WIP]: configs files folder

---
+++ rover/configs/local_env_vars.sh
@@ -0,0 +1,14 @@
+# -----------------------------------------------------------------------------
+# NOTE: PLEASE VISIT THE NEXT NOTION TO DECLARE YOUR VARIABLES:
+#   https://www.notion.so/kiwi/Defining-Environment-Variables-729fdef098a44b4e8e4c7cfef9a47abe
+#
+# DO NOT forget group your variables to keep order, format should be:
+#   export PACKGE/FUNCTION_VARIABLE-NAME=VALUE  # [type] values: short description
+# Example:
+#   export GUI_SENSORS_DISTANCE=1 # [int-bolean](1):Enable/(0):Disable - Distance sensors
+
+# -----------------------------------------------------------------------------
+export KIWIBOT_ID=1206     # [int] Robot ID
+                           # NOTE: Should be undefined in the robot
+export LOCAL_LAUNCH=1      # [int-bolean] Disable(1)/Enable(0) - Local launch mode (For pc launch)
+                           # NOTE: Should be zero in the robot
