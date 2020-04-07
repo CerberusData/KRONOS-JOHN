497074382c208a1fb1e8a0edfcaf4431a905dc9f
JohnBetaCode <john.betancourt93@gmail.com>
2020-04-07 15:08:50 -0500

    [WIP]: camera publishers

--- rover/ros2/src/vision/vision/video_mapping.py
+++ rover/ros2/src/vision/vision/video_mapping.py
@@ -8,6 +8,9 @@ Code Information:
 """
 
 # =============================================================================
+from functools import partial 
+import time
+import cv2
 import os
 
 import rclpy
@@ -16,9 +19,12 @@ from rclpy.logging import get_logger
 
 from vision.utils.cam_handler import read_cams_configuration
 from vision.utils.cam_handler import CamerasSupervisor
+from vision.utils.vision_utils import show_local_gui
+
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
 from usr_srv.srv import CamerasStatus
-from cv_bridge import CvBridge, CvBridgeError
 
 # =============================================================================
 class VideoPublishers(Node):
@@ -28,8 +34,6 @@ class VideoPublishers(Node):
 
         # ---------------------------------------------------------------------
         self._LOCAL_RUN = int(os.getenv(key="LOCAL_LAUNCH", default=0)) 
-        self._VIDEO_HEIGHT = int(os.getenv(key="VIDEO_HEIGHT", default=360)) 
-        self._VIDEO_WIDTH = int(os.getenv(key="VIDEO_WIDTH", default=640)) 
         self._FR_AGENT = int(os.getenv(key="FR_AGENT", default=0)) 
         self._CONF_PATH = str(os.getenv(key="CONF_PATH", 
             default=os.path.dirname(os.path.abspath(__file__))))
@@ -47,6 +51,8 @@ class VideoPublishers(Node):
         
         # Start cameras handler with configuration
         self.cameras_supervisor = CamerasSupervisor(cams_config=self.cams_config)
+        cams_status = self.cameras_supervisor.get_cameras_status()
+        self.img_bridge = CvBridge()
         
         # ---------------------------------------------------------------------
         # Services
@@ -55,21 +61,52 @@ class VideoPublishers(Node):
         
         # ---------------------------------------------------------------------
         # Publishers
-        #self.publisher_ = self.create_publisher(String, 'topic', 10)
-        # timer_period = 0.5  # seconds
-        #self.timer = self.create_timer(timer_period, self.timer_callback)
-        #self.i = 0
+        self.cam_publishers = {}
+        if self._FR_AGENT or self._LOCAL_RUN:
+            self.cam_publishers = {cam_label:self.create_publisher(Image, 
+                'streaming/cam_{}'.format(cam_label), 5) 
+                for cam_label in self.cams_config.keys()
+                if cams_status[cam_label] or self._LOCAL_RUN}        
+
+        # ---------------------------------------------------------------------
+        # Timers
+        self.cam_timers = {cam_label:self.create_timer(0.5, partial(
+            self.timer_callback, cam_label))
+            for cam_label in self.cams_config.keys()
+            if cams_status[cam_label] or self._LOCAL_RUN}
+
+        # ---------------------------------------------------------------------  
+        # Local gui
+        self.local_gui = local_gui()
 
     def cb_cams_status(self, request, response):
+
         response.cameras_status = ["{}:{}".format(cam_key, int(cam_status)
             ) for cam_key, cam_status in self.cameras_supervisor.get_cameras_status()]
         return response
 
-    def timer_callback(self):
-        msg = String()
-        msg.data = ""
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
+    def timer_callback(self, cam_label):
+
+        img = self.cameras_supervisor.camera_handlers[cam_label].image
+        if img is not None:
+            # Send supervisor's image message to topic
+            try:
+                img_msg = self.img_bridge.cv2_to_imgmsg(
+                    cvim=img, 
+                    encoding="bgr8")
+                # img_msg.header.stamp = time.time()
+                self.cam_publishers[cam_label].publish(img_msg)
+            except CvBridgeError as e:
+                self.get_logger().error("publishing CAM{} image in topic, {}".format(
+                    cam_label, e))
+
+class local_gui():
+
+    def __init__(self):
+
+        self._LOCAL_RUN = int(os.getenv(key="LOCAL_LAUNCH", default=0)) 
+
+
 
 # =============================================================================
 def main(args=None):
@@ -77,8 +114,8 @@ def main(args=None):
     # Initialize ROS communications for a given context.
     rclpy.init(args=args)
 
-    # Execute work and block until the context associated with the executor 
-    # is shutdown.
+    # Execute work and block until the context associated with the 
+    # executor is shutdown.
     video_publishers = VideoPublishers()
     rclpy.spin(video_publishers)
 
