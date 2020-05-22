41f7b725504b42fb1bb5ad49c5c8749e0fcfa357
Camilo Alvis <camiloalvis@kiwibot.com>
2020-05-22 13:54:30 -0500

    [FIX]: Whole IMU node

--- rover/ros2/src/bosch_imu/bosch_imu/imu_node.py
+++ rover/ros2/src/bosch_imu/bosch_imu/imu_node.py
@@ -158,9 +158,9 @@ class ImuNode(Node):
         self._pitch = 0.0
         self._yaw = 0.0
 
-        # Magnetic field for Berkeley - For Medellin it is -8.2
+        # Magnetic field for Berkeley - For Medellin it is -8.2 , -14.3
         self._mgn_decl = float(
-            os.getenv("IMU_MAGNETIC_DECLINATION", -14.3)) * (math.pi / 180.0)
+            os.getenv("IMU_MAGNETIC_DECLINATION", -8.2)) * (math.pi / 180.0)
         self._move_factor = float(os.getenv("IMU_MOVING_FACTOR", 0.014))
 
         # Open Serial Port
@@ -278,7 +278,6 @@ class ImuNode(Node):
 
         #print("Pitch: %0.4f" % pitch)
 
-
         # Yaw angle
         siny_cosp = 2 * ((w * z) + (x * y))
         cosy_cosp = 1 - (2 * ((y * y) + (z * z)))
@@ -355,29 +354,64 @@ class ImuNode(Node):
 
         if buf != 0:
 
-            # ------------------------------------------------------------ #
+            # --------------------------------------------------------------- #
             # Gravity values from the IMU buffer
-            gravity_x = float(st.unpack('h', st.pack('BB', buf[38], buf[39]))[0]) / 100.0
-            gravity_y = float(st.unpack('h', st.pack('BB', buf[40], buf[41]))[0]) / 100.0
-            gravity_z = float(st.unpack('h', st.pack('BB', buf[42], buf[43]))[0]) / 100.0
+            gravity_x = float(st.unpack('h', st.pack(
+                'BB', buf[38], buf[39]))[0]) / 100.0
+            gravity_y = float(st.unpack(
+                'h', st.pack('BB', buf[40], buf[41]))[0]) / 100.0
+            gravity_z = float(st.unpack(
+                'h', st.pack('BB', buf[42], buf[43]))[0]) / 100.0
 
-            # ------------------------------------------------------------ #
+            # --------------------------------------------------------------- #
+            # Raw data
+            self.imu_raw_msg.header.stamp = rclpy.clock.Clock().now().to_msg()
+            self.imu_raw_msg.header.frame_id = self.frame_id
+            self.imu_raw_msg.orientation_covariance[0] = -1
+
+            self.imu_raw_msg.linear_acceleration.x = float(st.unpack(
+                'h', st.pack('BB', buf[0], buf[1]))[0]) / self.acc_fact_
+            self.imu_raw_msg.linear_acceleration.y = float(st.unpack(
+                'h', st.pack('BB', buf[2], buf[3]))[0]) / self.acc_fact_
+            self.imu_raw_msg.linear_acceleration.z = float(st.unpack(
+                'h', st.pack('BB', buf[4], buf[5]))[0]) / self.acc_fact_
+            self.imu_raw_msg.linear_acceleration_covariance[0] = -1
+
+            self.imu_raw_msg.angular_velocity.x = float(st.unpack(
+                'h', st.pack('BB', buf[12], buf[13]))[0]) / self.gyr_fact_
+            self.imu_raw_msg.angular_velocity.y = float(st.unpack(
+                'h', st.pack('BB', buf[14], buf[15]))[0]) / self.gyr_fact_
+            self.imu_raw_msg.angular_velocity.z = float(st.unpack(
+                'h', st.pack('BB', buf[16], buf[17]))[0]) / self.gyr_fact_
+            self.imu_raw_msg.angular_velocity_covariance[0] = -1
+
+            # --------------------------------------------------------------- #
             # Filtered data
             self.imu_msg.header.stamp = rclpy.clock.Clock().now().to_msg()
             self.imu_msg.header.frame_id = self.frame_id
-            self.imu_msg.orientation.w = float(st.unpack('h', st.pack('BB', buf[24], buf[25]))[0])
-            self.imu_msg.orientation.x = float(st.unpack('h', st.pack('BB', buf[26], buf[27]))[0])
-            self.imu_msg.orientation.y = float(st.unpack('h', st.pack('BB', buf[28], buf[29]))[0])
-            self.imu_msg.orientation.z = float(st.unpack('h', st.pack('BB', buf[30], buf[31]))[0])
+            self.imu_msg.orientation.w = float(st.unpack('h', st.pack(
+                'BB', buf[24], buf[25]))[0])
+            self.imu_msg.orientation.x = float(st.unpack('h', st.pack(
+                'BB', buf[26], buf[27]))[0])
+            self.imu_msg.orientation.y = float(st.unpack('h', st.pack(
+                'BB', buf[28], buf[29]))[0])
+            self.imu_msg.orientation.z = float(st.unpack('h', st.pack(
+                'BB', buf[30], buf[31]))[0])
 
-            self.imu_msg.linear_acceleration.x = float(st.unpack('h', st.pack('BB', buf[32], buf[33]))[0]) / self.acc_fact_ + gravity_x
-            self.imu_msg.linear_acceleration.y = float(st.unpack('h', st.pack('BB', buf[34], buf[35]))[0]) / self.acc_fact_ + gravity_y
-            self.imu_msg.linear_acceleration.z = float(st.unpack('h', st.pack('BB', buf[36], buf[37]))[0]) / self.acc_fact_ + gravity_z
+            self.imu_msg.linear_acceleration.x = float(st.unpack('h', st.pack(
+                'BB', buf[32], buf[33]))[0]) / self.acc_fact_ + gravity_x
+            self.imu_msg.linear_acceleration.y = float(st.unpack('h', st.pack(
+                'BB', buf[34], buf[35]))[0]) / self.acc_fact_ + gravity_y
+            self.imu_msg.linear_acceleration.z = float(st.unpack('h', st.pack(
+                'BB', buf[36], buf[37]))[0]) / self.acc_fact_ + gravity_z
             self.imu_msg.linear_acceleration_covariance[0] = -1
 
-            self.imu_msg.angular_velocity.x = float(st.unpack('h', st.pack('BB', buf[12], buf[13]))[0]) / self.gyr_fact_
-            self.imu_msg.angular_velocity.y = float(st.unpack('h', st.pack('BB', buf[14], buf[15]))[0]) / self.gyr_fact_
-            self.imu_msg.angular_velocity.z = float(st.unpack('h', st.pack('BB', buf[16], buf[17]))[0]) / self.gyr_fact_
+            self.imu_msg.angular_velocity.x = float(st.unpack('h', st.pack(
+                'BB', buf[12], buf[13]))[0]) / self.gyr_fact_
+            self.imu_msg.angular_velocity.y = float(st.unpack('h', st.pack(
+                'BB', buf[14], buf[15]))[0]) / self.gyr_fact_
+            self.imu_msg.angular_velocity.z = float(st.unpack('h', st.pack(
+                'BB', buf[16], buf[17]))[0]) / self.gyr_fact_
             self.imu_msg.angular_velocity_covariance[0] = -1
 
             # print("X: %0.4f" % self.imu_msg.orientation.x, \
@@ -385,7 +419,7 @@ class ImuNode(Node):
             #     "Z: %0.4f" % self.imu_msg.orientation.z, \
             #     "W: %0.4f" % self.imu_msg.orientation.w)
 
-            # ------------------------------------------------------------ #
+            # --------------------------------------------------------------- #
             # Quaternion value allocation
             quaternion_orig = [
                 self.imu_msg.orientation.x,
@@ -393,6 +427,20 @@ class ImuNode(Node):
                 self.imu_msg.orientation.z,
                 self.imu_msg.orientation.w
             ]
+
+            # --------------------------------------------------------------- #
+            # Magnetometer data
+            self.mag_msg.header.stamp = rclpy.clock.Clock().now().to_msg()
+            self.mag_msg.header.frame_id = self.frame_id
+            self.mag_msg.magnetic_field.x = float(st.unpack('h', st.pack(
+                'BB', buf[6], buf[7]))[0]) / self.mag_fact_
+            self.mag_msg.magnetic_field.y = float(st.unpack('h', st.pack(
+                'BB', buf[8], buf[9]))[0]) / self.mag_fact_
+            self.mag_msg.magnetic_field.z = float(st.unpack('h', st.pack(
+                'BB', buf[10], buf[11]))[0]) / self.mag_fact_
+ 
+            # --------------------------------------------------------------- #
+            # Kiwibot filtered data
             """
                 For kiwibot the axes are reassigned as follows due to the
                 orientation of the IMU in the robot.
@@ -404,27 +452,130 @@ class ImuNode(Node):
 
             self.imu_bot_msg.header.stamp = rclpy.clock.Clock().now().to_msg()
             self.imu_bot_msg.header.frame_id = self.frame_id
-            self.imu_bot_msg.linear_acceleration.x = self.imu_msg.linear_acceleration.z - gravity_z
-            self.imu_bot_msg.linear_acceleration.y = self.imu_msg.linear_acceleration.x - gravity_x
-            self.imu_bot_msg.linear_acceleration.z = self.imu_msg.linear_acceleration.y - gravity_y
-            self.imu_bot_msg.linear_acceleration_covariance = [3e-1, 0.0, 0.0, 0.0, 3e-1, 0.0, 0.0, 0.0, 3e-1]
+
+            self.imu_bot_msg.linear_acceleration.x = \
+                self.imu_msg.linear_acceleration.z - gravity_z
+            self.imu_bot_msg.linear_acceleration.y = \
+                self.imu_msg.linear_acceleration.x - gravity_x
+            self.imu_bot_msg.linear_acceleration.z = \
+                self.imu_msg.linear_acceleration.y - gravity_y
+            self.imu_bot_msg.linear_acceleration_covariance = [
+                3e-1, 0.0, 0.0, 
+                0.0, 3e-1, 0.0, 
+                0.0, 0.0, 3e-1]
             self.imu_bot_msg.angular_velocity.x = self.imu_msg.angular_velocity.z
             self.imu_bot_msg.angular_velocity.y = self.imu_msg.angular_velocity.x
             self.imu_bot_msg.angular_velocity.z = self.imu_msg.angular_velocity.y
-            self.imu_bot_msg.angular_velocity_covariance = [1e-6, 0.0, 0.0, 0.0, 1e-6, 0.0, 0.0, 0.0, 1e-6]
+            self.imu_bot_msg.angular_velocity_covariance = [
+                1e-6, 0.0, 0.0, 
+                0.0, 1e-6, 0.0, 
+                0.0, 0.0, 1e-6]
 
-            # Obtaining current euler angles (Roll, Pitch, Yaw) of the Kiwibot
-            roll, pitch, yaw = self._euler_from_quaternion(
-                quaternion_orig[0], quaternion_orig[1], quaternion_orig[2], quaternion_orig[3]
-                )
-            #print("Roll: %0.4f" % roll, "Pitch: %0.4f" % pitch, "Yaw: %0.4f" % yaw)
-
-            angles = euler_from_quaternion(quaternion=quaternion_orig, axes='rxyz')
+            (pitch, roll, yaw) = euler_from_quaternion(
+                quaternion=quaternion_orig, axes='rxyz')
             
-            print("Roll 1: %0.4f" % roll, "Roll 2: %0.4f" % angles[0]) 
-            print("Pitch 1: %0.4f" % pitch, "Pitch 2: %0.4f" % angles[1]) 
-            print("Yaw 1: %0.4f" % yaw, "Yaw 2: %0.4f" % angles[2]) 
+            print("Pitch - X: %0.4f" % pitch) 
+            print("Roll - Y: %0.4f" % roll) 
+            print("Yaw - Z: %0.4f" % yaw) 
             print("# ========== #")
+
+            # --------------------------------------------------------------- #
+            # Finding the current azimuth (Yaw)
+            """
+                Azimuth is the angle (Between the current heading and the magnetic 
+                north) in the horizontal plane
+            """
+
+            current_azimuth = \
+                (math.atan2(self.mag_msg.magnetic_field.x, self.mag_msg.magnetic_field.z) - (math.pi / 2))
+
+            if (self._azimuth != 0.0):
+                current_azimuth = \
+                    ((current_azimuth - self._azimuth) + math.pi) % (2.0 * math.pi) - math.pi + self._azimuth
+                self._azimuth = ((1.0 - self._alpha) * current_azimuth) + (self._alpha * self._azimuth)
+
+                if (self._counter < 305):
+                    self._counter += 1
+            else:
+
+                if (self.mag_msg.magnetic_field.x != 0.0):
+                    self._azimuth = current_azimuth
+                
+                # Else condition to handle an error in the IMU when calculating the orientation
+            
+            if ((not self._first_orient) and (self._azimuth != 0.0) and (self._counter > 300)):
+                self._initial_orient  = -self._azimuth + self._mgn_decl - (self._yaw - math.pi)
+                self._alpha = 0.75
+                self._first_orient = True
+
+            # --------------------------------------------------------------- #
+            # Quaternion bot
+            """
+                This quaternion us created from the current euler angles. It is 
+                performed after the orientation correction.
+            """
+    
+            quaternion_bot = self._quaternion_from_euler(
+                -pitch, roll - (math.pi / 2), yaw - math.pi + self._initial_orient)
+    
+            # Kiwibot filtered data
+            self.imu_bot_msg.orientation.x = quaternion_bot[0]
+            self.imu_bot_msg.orientation.y = quaternion_bot[1]
+            self.imu_bot_msg.orientation.z = quaternion_bot[2]
+            self.imu_bot_msg.orientation.w = quaternion_bot[3]
+            self.imu_bot_msg.orientation_covariance = [
+                1e-5, 0.0, 0.0, 
+                0.0, 1e-5, 0.0, 
+                0.0, 0.0, 1e-3]
+
+            # --------------------------------------------------------------- #
+            # Quaternion bot for EKF
+            quaternion_bot_ekf = self._quaternion_from_euler(
+                -pitch, roll - (math.pi / 2), -self._azimuth + self._mgn_decl)
+    
+            # Kiwibot filtered data for EKF
+            self.imu_ekf_msg.header.stamp =rclpy.clock.Clock().now().to_msg()
+            self.imu_ekf_msg.header.frame_id = self.frame_id
+            self.imu_ekf_msg.orientation.x = quaternion_bot_ekf[0]
+            self.imu_ekf_msg.orientation.y = quaternion_bot_ekf[1]
+            self.imu_ekf_msg.orientation.z = quaternion_bot_ekf[2]
+            self.imu_ekf_msg.orientation.w = quaternion_bot_ekf[3]
+            self.imu_ekf_msg.orientation_covariance = [
+                1e-3, 0.0, 0.0, 
+                0.0, 1e-3, 0.0, 
+                0.0, 0.0, 1e-2]
+    
+            # --------------------------------------------------------------- #
+            # Publish IMU motion status
+    
+            self._acc_values = np.append(
+                self._acc_values, abs(self.imu_bot_msg.linear_acceleration.x))
+    
+            self._move_counter += 1
+            if (self._move_counter == 50):
+    
+                # Check if the average linear acceletarion was greater than the motion factor
+                moving_now = np.mean(self._acc_values) > self._move_factor
+    
+                if (moving_now != self._moving_old):
+                    self.move_msg.data = bool(moving_now)
+                    self._move_pub = True
+                
+                self._moving_old = moving_now
+                self._move_pub = False           
+                self._acc_values = np.array([])
+                self._move_counter = 0   
+    
+            # --------------------------------------------------------------- #
+            # Temperature data
+            self.tmp_msg.header.stamp = rclpy.clock.Clock().now().to_msg()
+            self.tmp_msg.header.frame_id = self.frame_id
+            self.tmp_msg.temperature = float(buf[44])
+    
+            self.status_msg.data = "IMU Ok"
+    
+        else:
+            self.status_msg.data = "Unavailable data"
 
 
     def cb_publish_imu(self):
@@ -434,7 +585,27 @@ class ImuNode(Node):
 
         # Function for updating IMU values
         self._update_imu_data()
-        
+
+        # Raw data publisher
+        self.pub_imu_raw.publish(self.imu_raw_msg)
+    
+        # Filtered data publisher
+        if (self._counter > 300):
+            self.pub_bot_data.publish(self.imu_bot_msg)
+            self.pub_bot_ekf.publish(self.imu_ekf_msg)
+    
+        # Movement publisher
+        if (self._move_pub == True):
+            self.pub_bot_move.publish(self.move_msg)
+    
+        # Magnetometer data publisher
+        self.pub_imu_mag.publish(self.mag_msg)
+    
+        # Temperature data publisher
+        self.pub_imu_temp.publish(self.tmp_msg)
+    
+        # IMU status publisher
+        self.pub_imu_status.publish(self.status_msg)
         
 
 def main(args = None):
