6f585d66aa5cb3a03ca8be724b61a4474b0c097e
Camilo Alvis <kmilo7204@gmail.com>
2020-07-08 15:19:26 -0500

    [WIP]: Wheel odometry

--- rover/ros2/src/wheel_odometry/src/wheel_odometry.cpp
+++ rover/ros2/src/wheel_odometry/src/wheel_odometry.cpp
@@ -27,6 +27,12 @@ WheelOdometry::WheelOdometry(const rclcpp::NodeOptions & options)
     prev_time_ = this->now();
     pub_timer_ = this->create_wall_timer(std::chrono::milliseconds(50), 
         std::bind(&WheelOdometry::PubTimerCb, this));
+
+    local_wheel_odom_msg_.header.frame_id = "local_odom";
+    local_wheel_odom_msg_.child_frame_id = "base_link";    
+
+    global_wheel_odom_msg_.header.frame_id = "global_odom";
+    global_wheel_odom_msg_.child_frame_id = "base_link";
 }
 
 /* ------------------------------------------------------------------------- */
@@ -111,9 +117,7 @@ void WheelOdometry::ImuCb(const sensor_msgs::msg::Imu::SharedPtr msg)
     
     if (imu_published_ == false)
     {
-        /*
-            It checks if the IMU has published messages.
-        */
+        // IMU state must be OK
         imu_published_ = true;
     }
 
@@ -127,6 +131,13 @@ void WheelOdometry::ImuCb(const sensor_msgs::msg::Imu::SharedPtr msg)
 // Functions
 float WheelOdometry::CalculateSlipFactor(float kin_omega, float imu_omega)
 {
+    /*
+        Difference calculation between the kinematic and IMU angular speed.
+    */
+    RCLCPP_INFO(this->get_logger(), 
+        "Kinematic: %0.4f, IMU: %0.4f", 
+        kin_omega, imu_omega);
+
     float alpha = 1.0f;
     if (kin_omega != 0.0f)
     {
@@ -145,16 +156,17 @@ void WheelOdometry::CalculateOdometry()
     */
 
     // Wheels linear velocities
-    float _FR_vel = (2.0 * PI * wheel_rad_ * motors_rpm_[0]) / 60.0f; 
-    float _RR_vel = (2.0 * PI * wheel_rad_ * motors_rpm_[1]) / 60.0f; 
-    float _RL_vel = (2.0 * PI * wheel_rad_ * motors_rpm_[2]) / 60.0f; 
-    float _FL_vel = (2.0 * PI * wheel_rad_ * motors_rpm_[3]) / 60.0f; 
+    float FR_vel = (2.0 * PI * wheel_rad_ * motors_rpm_[0]) / 60.0f; 
+    float RR_vel = (2.0 * PI * wheel_rad_ * motors_rpm_[1]) / 60.0f; 
+    float RL_vel = (2.0 * PI * wheel_rad_ * motors_rpm_[2]) / 60.0f; 
+    float FL_vel = (2.0 * PI * wheel_rad_ * motors_rpm_[3]) / 60.0f; 
 
     // Left and Right linear velocities
-    float _R_vel = -(_FR_vel + _RR_vel) / 2.0f;  
-    float _L_vel = (_FL_vel + _RL_vel) / 2.0f;
-    float _X_vel = (_R_vel + _L_vel) / 2.0f;
-    float _kin_omega = (_R_vel - _L_vel) / bot_track_;
+    float R_vel = -(FR_vel + RR_vel) / 2.0f;  
+    float L_vel = (FL_vel + RL_vel) / 2.0f;
+    // Lineal and angular velocities
+    float X_vel = (R_vel + L_vel) / 2.0f;
+    float kin_omega = (R_vel - L_vel) / bot_track_;
 
     rclcpp::Time curr_time = this->now();
     double dt = (curr_time - prev_time_).seconds();
@@ -166,19 +178,17 @@ void WheelOdometry::CalculateOdometry()
 
     // Header assignation
     local_wheel_odom_msg_.header.stamp = this->now();
-    local_wheel_odom_msg_.header.frame_id = "odom";
-    local_wheel_odom_msg_.child_frame_id = "base_link";
     // Quaternion assignation
     local_wheel_odom_msg_.pose.pose.orientation.x = quat[0];
     local_wheel_odom_msg_.pose.pose.orientation.y = quat[1];
     local_wheel_odom_msg_.pose.pose.orientation.z = quat[2];
     local_wheel_odom_msg_.pose.pose.orientation.w = quat[3];
     // Speeds in X and Y axes
-    float _X_dot = _X_vel * cos(imu_yaw_ - imu_yaw_offset_);
-    float _Y_dot = _X_vel * sin(imu_yaw_ - imu_yaw_offset_);
+    float X_dot = X_vel * cos(imu_yaw_ - imu_yaw_offset_);
+    float Y_dot = X_vel * sin(imu_yaw_ - imu_yaw_offset_);
     // Adding displacement in [m] to the current message
-    local_wheel_odom_msg_.pose.pose.position.x += _X_dot * dt;
-    local_wheel_odom_msg_.pose.pose.position.y += _Y_dot * dt;
+    local_wheel_odom_msg_.pose.pose.position.x += X_dot * dt;
+    local_wheel_odom_msg_.pose.pose.position.y += Y_dot * dt;
     local_wheel_odom_msg_.pose.pose.position.z = 0.0f;
     local_wheel_odom_msg_.pose.covariance = {0.1, 0, 0, 0, 0, 0,
                                         0, 0.1, 0, 0, 0, 0,
@@ -187,7 +197,7 @@ void WheelOdometry::CalculateOdometry()
                                         0, 0, 0, 0, 0.001, 0,
                                         0, 0, 0, 0, 0, 0.002};
     // Velocities assignation
-    local_wheel_odom_msg_.twist.twist.linear.x = _X_vel;
+    local_wheel_odom_msg_.twist.twist.linear.x = X_vel;
     local_wheel_odom_msg_.twist.twist.linear.y = 0.0f;
     local_wheel_odom_msg_.twist.twist.linear.z = 0.0f;
     local_wheel_odom_msg_.twist.twist.angular.x = 0.0f;
@@ -196,14 +206,14 @@ void WheelOdometry::CalculateOdometry()
 
     // Testing purposes
     local_wheel_odom_msg_.twist.covariance = {0.0000001, 0, 0, 0, 0, 0,
-                                    0, 0.000000001, 0, 0, 0, 0,
-                                    0, 0, 0.00000002, 0, 0, 0,
-                                    0, 0, 0, 0.01, 0, 0,
-                                    0, 0, 0, 0, 0.01, 0,
-                                    0, 0, 0, 0, 0, 0.002};
+                                        0, 0.000000001, 0, 0, 0, 0,
+                                        0, 0, 0.00000002, 0, 0, 0,
+                                        0, 0, 0, 0.01, 0, 0,
+                                        0, 0, 0, 0, 0.01, 0,
+                                        0, 0, 0, 0, 0, 0.002};
 
     /* Twist covariance tunning (Velocities) */
-    // if ((imu_state_ == true) && _X_vel == 0.0)
+    // if ((imu_state_ == true) && X_vel == 0.0)
     // { //Robot is moving but wheels not (e.g. robot is being carried)
     //     /* 
     //         When robot is moving, but wheels not, hence we should avoid adding
@@ -234,31 +244,29 @@ void WheelOdometry::CalculateOdometry()
     
 
     // Calculations for Global Wheel Odometry
-    float _alpha = CalculateSlipFactor(_kin_omega, imu_omega_);
-    float _X_vel_corrected = _X_vel * _alpha;
+    float alpha = CalculateSlipFactor(kin_omega, imu_omega_);
+    float X_vel_corrected = X_vel * alpha;
 
     // Filling a global Quaternion with the current euler angles
     tf2::Quaternion quat_global;
     quat_global.setRPY(imu_roll_, imu_pitch_, imu_yaw_);
     // Header assignation
     global_wheel_odom_msg_.header.stamp = this->now();
-    global_wheel_odom_msg_.header.frame_id = "odom";
-    global_wheel_odom_msg_.child_frame_id = "base_link";
     // Quaternion assignation
     global_wheel_odom_msg_.pose.pose.orientation.x = quat_global[0];
     global_wheel_odom_msg_.pose.pose.orientation.y = quat_global[1];
     global_wheel_odom_msg_.pose.pose.orientation.z = quat_global[2];
     global_wheel_odom_msg_.pose.pose.orientation.w = quat_global[3];
     // Speeds in X and Y axes
-    float _X_dot_global = _X_vel_corrected * cos(imu_yaw_ - imu_yaw_offset_);
-    float _Y_dot_global = _X_vel_corrected * sin(imu_yaw_ - imu_yaw_offset_);
+    float X_dot_global = X_vel_corrected * cos(imu_yaw_ - imu_yaw_offset_);
+    float Y_dot_global = X_vel_corrected * sin(imu_yaw_ - imu_yaw_offset_);
     // Adding displacement in [m] to the global message
-    global_wheel_odom_msg_.pose.pose.position.x += _X_dot_global * dt;
-    global_wheel_odom_msg_.pose.pose.position.y += _Y_dot_global * dt;
+    global_wheel_odom_msg_.pose.pose.position.x += X_dot_global * dt;
+    global_wheel_odom_msg_.pose.pose.position.y += Y_dot_global * dt;
     global_wheel_odom_msg_.pose.pose.position.z = 0.0f;
     global_wheel_odom_msg_.pose.covariance = local_wheel_odom_msg_.pose.covariance;
     // Velocities assignation
-    global_wheel_odom_msg_.twist.twist.linear.x = _X_vel_corrected;
+    global_wheel_odom_msg_.twist.twist.linear.x = X_vel_corrected;
     global_wheel_odom_msg_.twist.twist.linear.y = 0.0f;
     global_wheel_odom_msg_.twist.twist.linear.z = 0.0f;
     global_wheel_odom_msg_.twist.twist.angular.x = 0.0f;
