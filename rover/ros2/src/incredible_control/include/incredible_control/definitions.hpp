a3025819bdab9ec16077d30a027c04d811a0aa53
Camilo Alvis <kmilo7204@gmail.com>
2020-07-13 16:00:25 -0500

    [WIP]: Adding function to calculate the Robot position

--- rover/ros2/src/incredible_control/include/incredible_control/definitions.hpp
+++ rover/ros2/src/incredible_control/include/incredible_control/definitions.hpp
@@ -1,4 +1,6 @@
 #include <chrono>
+#include <vector>
+#include <math.h>
 
 // ROS 2 Default
 #include "rclcpp/rclcpp.hpp"
@@ -7,20 +9,21 @@
 #include "std_msgs/msg/float32.hpp"
 
 // Custom messages
-//#include "usr_msgs/msg/best_message.hpp"
+//#include "usr_msgs/msg/robot_position.hpp"
 
 using std::placeholders::_1;
 
 class Control : public rclcpp::Node
 {
 public: 
-    Control(const rclcpp::NodeOptions & options);
+    Control(const rclcpp::NodeOptions & options, double angle);
     ~Control(){};
+
 
 // private:
     /*
         Name here both publishers, one with the std_msgs [Float32] and other 
-        with usr_msgs [BestMessage]
+        with usr_msgs [RobotPosition]
 
         Find more information about creating publishers:
             https://index.ros.org/doc/ros2/Tutorials/Writing-A-Simple-Cpp-Publisher-And-Subscriber/
@@ -29,12 +32,26 @@ public:
             rclcpp::Publisher<message_type>::SharedPtr pub_name;
     */
 
-
+// Timers
 private: 
     rclcpp::TimerBase::SharedPtr pub_tmr_;
 
+// Member functions
 private:
+    
+    /*
+        Publisher time. This is initially setup at 500ms
+    */
     void PubTimerCb();
+    
+    /*
+        This function will calculate the Robot position based on the unit circle
+    */
+    std::vector<double> CalculatePosition(double angle);
 
+// Member attributes 
+private:
+    double m_angle;
+    std::vector<double> m_pos = {0.0, 0.0};
 };
 
--- rover/ros2/src/incredible_control/src/main.cpp
+++ rover/ros2/src/incredible_control/src/main.cpp
@@ -1,12 +1,12 @@
 #include "incredible_control/definitions.hpp"
 
-Control::Control(const rclcpp::NodeOptions & options) 
-: Node("incredible_control", options)
+Control::Control(const rclcpp::NodeOptions & options, double angle) 
+: Node("incredible_control", options), m_angle(angle)
 {
     /*
         Define here both publishers publishers
         
-        Find more information:
+        Find more informations:
             https://index.ros.org/doc/ros2/Tutorials/Writing-A-Simple-Cpp-Publisher-And-Subscriber/
 
         Example:
@@ -17,6 +17,7 @@ Control::Control(const rclcpp::NodeOptions & options)
     pub_tmr_ = this->create_wall_timer(
         std::chrono::milliseconds(500),
         std::bind(&Control::PubTimerCb, this));
+    
 }
 
 void Control::PubTimerCb()
@@ -26,20 +27,40 @@ void Control::PubTimerCb()
         
         Example for message definition: 
             auto msg = std::make_unique<msg_type>();
+            msg->data = value;
             pub_name->publish(msg);
 
     */
     
-    RCLCPP_INFO(this->get_logger(),
+    RCLCPP_WARN(this->get_logger(),
         "Publishing from an incredible_control Node!!!");
 
+    m_pos = CalculatePosition(m_angle);
+
+    RCLCPP_INFO(this->get_logger(),
+        "Pos X: %0.2f, Pos Y: %0.2f", m_pos[0], m_pos[1]);
+
+    m_angle += 0.1047;
+    m_angle = m_angle >= 2*M_PI ? 0.0 : m_angle;
 }
+
+std::vector<double> Control::CalculatePosition(double angle)
+{
+    double x = cos(angle);
+    double y = sin(angle);
+
+    std::vector<double> pos = {x, y};
+    
+    return pos;
+}
+
+
 
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
     rclcpp::NodeOptions options;
-    auto node = std::make_shared<Control>(options);
+    auto node = std::make_shared<Control>(options, 1.57);
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
